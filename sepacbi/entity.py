fca8a69ffccfc5f134eaa74984e5253da38759fb
Antonio Parrella <zidagartsk@gmail.com>
2024-05-28 17:58:04 +0200

    test

--- sepacbi/entity.py
+++ sepacbi/entity.py
@@ -28,29 +28,39 @@ class Address(AttributeCarrier):
     """
     The postal address of an entity.
     """
-    def __init__(self, *args):
+    def __init__(self, address=None, country=None, town=None, *args):
         """
         The argument processing is NOT based on keyword parameters, so we
         cannot inherit from AttributeCarrier.
         """
         # pylint: disable=super-init-not-called
-        self.lines = args
+        self.lines = address
+        self.country = country
+        self.town = town
 
     def perform_checks(self):
         "Check the length of each line."
-        line_count = len(self.lines)
-        if line_count < 1 or line_count > 2:
-            raise AddressFormatError('Must have either 1 or 2 address lines')
-        for line in self.lines:
-            if len(line) < 1 or len(line) > 70:
-                raise AddressFormatError('Line must have length '
-                                         'between 1 and 70 characters')
+        if self.lines is not None:
+            if not isinstance(self.lines, (list, tuple)):
+                raise AddressFormatError('Address format error')
+            line_count = len(self.lines)
+            if line_count < 1 or line_count > 2:
+                raise AddressFormatError('Must have either 1 or 2 address lines')
+            for line in self.lines:
+                if len(line) < 1 or len(line) > 70:
+                    raise AddressFormatError('Line must have length '
+                                            'between 1 and 70 characters')
 
     def emit_tag(self):
         "Emit the postal address with each of its lines."
         root = etree.Element('PstlAdr')
-        for line in self.lines:
-            etree.SubElement(root, 'AdrLine').text = line
+        if self.town is not None:
+            etree.SubElement(root, 'TwnNm').text = self.town
+        if self.country is not None:
+            etree.SubElement(root, 'Ctry').text = self.country
+        if self.lines is not None:
+            for line in self.lines:
+                etree.SubElement(root, 'AdrLine').text = line
         return root
 
 
@@ -72,10 +82,11 @@ class IdHolder(AttributeCarrier):
     Must have a CUC; may have an Italian tax code.
     """
     allowed_args = ('name', 'cf', 'code', 'private', 'cuc', 'address',
-                    'country', 'sia_code')
+                    'country', 'pstl_town', 'pstl_country', 'sia_code')
 
     def __init__(self, **kwargs):
         self.private = False
+        self.pstl = None
         super(IdHolder, self).__init__(**kwargs)
 
     def perform_checks(self):
@@ -89,12 +100,26 @@ class IdHolder(AttributeCarrier):
             self.max_length('cf', 16)
         if hasattr(self, 'cuc'):
             self.max_length('cuc', 35)
-        if hasattr(self, 'address'):
-            if isinstance(self.address, (list, tuple)):
-                self.address = Address(*self.address)
-            assert isinstance(self.address, Address)
+        if hasattr(self, 'pstl_country'):
+            self.length('pstl_country', 2)
+        if hasattr(self, 'pstl_town'):
+            self.max_length('pstl_town', 35)
         if hasattr(self, 'country'):
             self.length('country', 2)
+        if hasattr(self, 'pstl_country') or hasattr(self, 'pstl_country'):
+            pstl_country = None
+            pstl_town = None
+            address = None
+            if hasattr(self, 'pstl_country'):
+                pstl_country = self.pstl_country
+            if hasattr(self, 'pstl_town'):
+                pstl_town = self.pstl_town
+            if hasattr(self, 'address'):
+                address = self.address
+
+            self.pstl = Address(address, pstl_country,
+                                pstl_town)
+            assert isinstance(self.pstl, Address)
 
     def emit_tag(self, tag=None, as_initiator=False, hide=False):
         """
@@ -111,8 +136,8 @@ class IdHolder(AttributeCarrier):
             name.text = self.name
 
         # Address
-        if hasattr(self, 'address') and not as_initiator:
-            root.append(self.address.__tag__())
+        if self.pstl is not None and not as_initiator:
+            root.append(self.pstl.__tag__())
 
         # ID
         idtag = etree.SubElement(root, 'Id')
